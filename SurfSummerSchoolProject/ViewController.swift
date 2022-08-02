1a99e0b2e9138de50385695d941ab499877eabf9
vetka4ever <dansh@sfedu.ru>
2022-08-02 16:38:41 +0300

    Initial Commit

---
+++ SurfSummerSchoolProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Daniil on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
