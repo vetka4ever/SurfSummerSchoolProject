98ad22822e4bc5e422bad38f89985ec898088f81
vetka4ever <dansh@sfedu.ru>
2022-08-03 14:50:46 +0300

    added search button & navigation controller

--- SurfSummerSchoolProject/ViewController.swift
+++ SurfSummerSchoolProject/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var findButton: UIBarButtonItem{
+        let image = UIImage(systemName: "magnifyingglass")
+        let button = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(rightBarButtonSelector))
+        button.tintColor = .black
+        return button
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem  = findButton
+        self.title = "Navigation title"
+    }
+    
+    @objc func rightBarButtonSelector()
+    {
+        print("Search Button was tapped ")
     }
 
 
