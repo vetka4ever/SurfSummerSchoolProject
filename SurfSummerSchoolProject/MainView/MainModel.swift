e5760f80682f8bb77274a573b8677381a79edeb5
vetka4ever <dansh@sfedu.ru>
2022-08-05 15:20:05 +0300

    has created model for cell of collection

--- SurfSummerSchoolProject/MainView/MainModel.swift
+++ SurfSummerSchoolProject/MainView/MainModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+final class MainModel{
+    
+    //MARK: - Properties
+    var items: [ItemModel] = []
+    
+    // MARK: - Methods
+    func getPosts(){
+        items = Array(repeating: ItemModel.createDefault(), count: 100)
+    }
+    
+    struct ItemModel{
+        let image: UIImage?
+        let title: String
+        let isFavotire: Bool
+        let dateCreation: String
+        let content: String
+    
+
+    static func createDefault() -> ItemModel{
+        .init(image: UIImage(named: "default-image"), title: "Чашечка свежего кофе", isFavotire: false, dateCreation: "12.05.2022", content: "Для бариста и посетителей кофеен специальные кружки для кофе — это ещё один способ проконтролировать вкус напитка и приготовить его именно так, как нравится вам. \n Теперь, кроме регулировки экстракции, настройки помола, времени заваривания и многого что помогает выделять нужные характеристики кофе, вы сможете выбрать и кружку для кофе в зависимости от сорта.")
+    }
+        
+        
+        
+    }
+    
+}
+
--- SurfSummerSchoolProject/TabBar/TabBarConfigurator.swift
+++ SurfSummerSchoolProject/TabBar/TabBarConfigurator.swift
@@ -19,9 +19,6 @@ final class TabBarConfigurator{
         return getTaBarController()
     }
     
-    
-    
-   
 }
 
 private extension TabBarConfigurator {
